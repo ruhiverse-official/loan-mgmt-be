cbb5652184f559b0e795ee13ee50274143aa719d
Ruhiverse Official <reachus@ruhiverse.com>
2025-01-27 14:04:25 +0530

    Clear output before response

--- api/utils/Response.php
+++ api/utils/Response.php
@@ -1,9 +1,16 @@
-
 <?php
 
 class Response {
     public static function send($status, $message, $data = null) {
+        // Clear any existing output buffer
+        if (ob_get_length()) {
+            ob_clean();
+        }
+
+        // Set the Content-Type header
         header('Content-Type: application/json');
+
+        // Output the JSON response
         echo json_encode(['status' => $status, 'message' => $message, 'data' => $data]);
         exit();
     }
